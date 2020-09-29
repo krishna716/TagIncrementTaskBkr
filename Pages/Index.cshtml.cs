c3bb4eb18ee18f50ce6c5a9b348a0a532b9e02cb
krishna716 <krishb2000@hotmail.com>
2020-09-29 10:58:26 -0500

    Changed UI to include system variables

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.Extensions.Logging;
+
+//namespace TagIncrementAKSApp.Pages
+//{
+//	public class IndexModel : PageModel
+//	{
+//		private readonly ILogger<IndexModel> _logger;
+
+//		public IndexModel(ILogger<IndexModel> logger)
+//		{
+//			_logger = logger;
+//		}
+
+//		public void OnGet()
+//		{
+
+//		}
+//	}
+//}
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
